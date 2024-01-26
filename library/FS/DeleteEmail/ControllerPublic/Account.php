368c1a317f0113262844890791711638028e31cc
HaroonCheema <haroon051118@gmail.com>
2024-01-26 21:07:16 +0500

    ok

--- library/FS/DeleteEmail/ControllerPublic/Account.php
+++ library/FS/DeleteEmail/ControllerPublic/Account.php
@@ -2,13 +2,109 @@
 
 class FS_DeleteEmail_ControllerPublic_Account extends XFCP_FS_DeleteEmail_ControllerPublic_Account
 {
-    public function actionContactDetails()
+
+    /**
+     * Database object
+     *
+     * @var Zend_Db_Adapter_Abstract
+     */
+    protected $_db = null;
+
+    // public function actionContactDetails()
+    // {
+    //     $visitor = XenForo_Visitor::getInstance();
+
+    //     $auth = $this->_getUserModel()->getUserAuthenticationObjectByUserId($visitor['user_id']);
+    //     if (!$auth) {
+    //         return $this->responseNoPermission();
+    //     }
+
+    //     // echo "<pre>";
+    //     // var_dump($visitor['deleted_by']);
+    //     // exit;
+    //     // // echo "hello world";
+    //     // // exit;
+
+    //     $response = parent::actionContactDetails();
+
+    //     return $response;
+    // }
+
+    public function actionDeleteEmail()
     {
-        // echo "hello world";
+
+        // if ($this->isUpdate() && $this->isChanged('navigation_id'))
+        // {
+
+        $visitor = XenForo_Visitor::getInstance();
+
+        // echo "<pre>";
+        // var_dump($visitor);
         // exit;
 
-        $response = parent::actionContactDetails();
+        if ($this->isConfirmedPost()) {
+            $userId = $visitor['user_id'];
+            $email = '';
 
-        return $response;
+            $db = XenForo_Application::getDb();
+
+            $sql = 'UPDATE xf_user SET email = ? WHERE user_id = ?';
+            $db->query($sql, [$email, $userId]);
+
+            return $this->responseRedirect(
+                XenForo_ControllerResponse_Redirect::SUCCESS,
+                XenForo_Link::buildPublicLink('account/contact-details')
+            );
+        }
+
+        $viewParams = array(
+
+            'email' => $visitor->email,
+        );
+
+        return $this->responseView(
+            'FS_DeleteEmail_ControllerPublic_Account_Delete_Email',
+            'fs_delete_email_delete',
+            $viewParams
+        );
     }
+
+    // public function actionDeleteEmail()
+    // {
+    // 	$visitor = \XF::visitor();
+    // 	$auth = $visitor->Auth->getAuthenticationHandler();
+    // 	if (!$auth) {
+    // 		return $this->noPermission();
+    // 	}
+
+    // 	if ($this->isPost()) {
+    // 		$visitor->fastUpdate('email', '');
+
+    // 		return $this->redirect($this->buildLink('account/account-details'));
+    // 	}
+
+    // 	$viewpParams = [
+    // 		'confirmUrl' => $this->buildLink('account/delete-email', $visitor),
+    // 		'contentTitle' => $visitor->email,
+    // 	];
+
+    // 	return $this->view('XF\Account', 'fs_email_delete_confirm', $viewpParams);
+    // }
+
+    // public function actionEmail()
+    // {
+    //     $visitor = XenForo_Visitor::getInstance();
+
+    // 	$auth = $visitor->Auth->getAuthenticationHandler();
+    // 	if (!$auth) {
+    // 		return $this->noPermission();
+    // 	}
+
+    // 	if ($visitor['deleted_by'] == 1) {
+    // 		throw $this->exception(
+    // 			$this->error(\XF::phrase('your_email_may_not_be_changed_at_this_time'))
+    // 		);
+    // 	}
+    // 	return parent::actionEmail();
+    // }
 }
--- library/FS/DeleteEmail/Model/User.php
+++ library/FS/DeleteEmail/Model/User.php
@@ -3,32 +3,32 @@
 class FS_DeleteEmail_Model_User extends XFCP_FS_DeleteEmail_Model_User
 {
     
-    public function prepareUserOrderOptions(array &$fetchOptions, $defaultOrderSql = '')
-    {
+//     public function prepareUserOrderOptions(array &$fetchOptions, $defaultOrderSql = '')
+//     {
 
-            $choices = array(
-                    'username' => 'user.username',
-                    'register_date' => 'user.register_date',
-                    'message_count' => 'user.message_count',
-                    'trophy_points' => 'user.trophy_points',
-                    'like_count' => 'user.like_count',
-                    'last_activity' => 'user.last_activity',
-                    'deleted_by' =>  'user.deleted_by',
-            );
-            return $this->getOrderByClause($choices, $fetchOptions, $defaultOrderSql);
-    }
+//             $choices = array(
+//                     'username' => 'user.username',
+//                     'register_date' => 'user.register_date',
+//                     'message_count' => 'user.message_count',
+//                     'trophy_points' => 'user.trophy_points',
+//                     'like_count' => 'user.like_count',
+//                     'last_activity' => 'user.last_activity',
+//                     'deleted_by' =>  'user.deleted_by',
+//             );
+//             return $this->getOrderByClause($choices, $fetchOptions, $defaultOrderSql);
+//     }
     
-    public function prepareUserConditions(array $conditions, array &$fetchOptions)
-    {
-        $parent = parent::prepareUserConditions($conditions, $fetchOptions);
+//     public function prepareUserConditions(array $conditions, array &$fetchOptions)
+//     {
+//         $parent = parent::prepareUserConditions($conditions, $fetchOptions);
         
-        if (!empty($conditions['deleted_by']) && is_array($conditions['deleted_by']))
-        {
-                $sqlConditions[] = $this->getCutOffCondition("user.deleted_by", $conditions['deleted_by']);
-                return $this->getConditionsForClause($sqlConditions);
-        }
+//         if (!empty($conditions['deleted_by']) && is_array($conditions['deleted_by']))
+//         {
+//                 $sqlConditions[] = $this->getCutOffCondition("user.deleted_by", $conditions['deleted_by']);
+//                 return $this->getConditionsForClause($sqlConditions);
+//         }
                 
-        return $parent;
-    }
+//         return $parent;
+//     }
 
 }
